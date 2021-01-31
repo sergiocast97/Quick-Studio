7fd2286dabecf6ecd5e81a84cb1b4624778dcf52
sergiocast97 <sergiocast97@gmail.com>
2021-01-31 14:32:23 +0000

    Switched to EJS, added routing and error handling

--- app.js
+++ app.js
@@ -9,9 +9,8 @@ var usersRouter = require('./routes/users');
 
 var app = express();
 
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
+// TODO: Change to VUE
+app.set('view engine', 'ejs');
 
 app.use(logger('dev'));
 app.use(express.json());
@@ -29,13 +28,14 @@ app.use(function(req, res, next) {
 
 // error handler
 app.use(function(err, req, res, next) {
+  
   // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
 
   // render the error page
   res.status(err.status || 500);
-  res.render('error');
+  res.render('pages/error', { error: err });
 });
 
 module.exports = app;
--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,75 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET home page. */
+// Show the Homepage
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('pages/index', { title: 'Express' });
 });
+
+// Show the Login Page
+router.get('/login', function (req, res) {
+  res.render('pages/login', { title: 'Login' });
+})
+
+// Show the Register Page
+router.get('/register', function (req, res) {
+  res.render('pages/register', { title: 'Register' });
+})
+
+// Show the Test Page
+router.get('/test', function (req, res) {
+  res.render('pages/test', { title: 'Test Page' });
+})
+
+
+/* Log in the user */
+router.post('/login', function(req, res){
+
+  authenticate(req.body.username, req.body.password, function(err, user){
+    if (user) {
+      // Regenerate session when signing in
+      // to prevent fixation
+      req.session.regenerate(function(){
+        // Store the user's primary key
+        // in the session store to be retrieved,
+        // or in this case the entire user object
+        req.session.user = user;
+        req.session.success = 'Authenticated as ' + user.name
+          + ' click to <a href="/logout">logout</a>. '
+          + ' You may now access <a href="/restricted">/restricted</a>.';
+        res.redirect('back');
+      });
+    } else {
+      req.session.error = 'Authentication failed, please check your '
+        + ' username and password.'
+        + ' (use "tj" and "foobar")';
+      res.redirect('/login');
+    }
+  });
+});
+
+/**
+ * Examples of Routes
+ */
+
+// Get Request (e.g Get a page)
+router.get('/', function (req, res) {
+  res.send('Hello World!')
+})
+
+// Post Request (Submit data)
+router.post('/', function (req, res) {
+  res.send('Got a POST request')
+})
+
+// Put request (Replace Data)
+router.put('/user', function (req, res) {
+  res.send('Got a PUT request at /user')
+})
+
+// Delete Request (Removes data)
+router.delete('/user', function (req, res) {
+  res.send('Got a DELETE request at /user')
+})
 
 module.exports = router;
