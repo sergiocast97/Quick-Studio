93d9a9c291da8a295a78921e74384389fe72d8ca
sergiocast97 <sergiocast97@gmail.com>
2021-02-05 13:43:05 +0000

    MVC Structure

---
+++ server.js
@@ -0,0 +1,31 @@
+// Libraries
+if(process.env.NODE_ENV !== 'production') {
+    require('dotenv')
+}
+const express = require('express')
+const app = express()
+const expressLayouts = require('express-ejs-layouts')
+const mongoose = require('mongoose')
+
+// Controller
+const indexRouter = require('./routes/index')
+
+// View
+app.set('view engine', 'ejs')
+app.set('views', __dirname + '/views')
+app.set('layout', 'layouts/layout')
+app.use(expressLayouts)
+app.use(express.static('public'))
+
+// Model
+mongoose.connect(process.env.DATABASE_URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+})
+const db = mongoose.connection
+db.on('error', error => console.error(error))
+db.once('open', () => console.log('Connected for mongoose'))
+
+// Startup
+app.use('/', indexRouter)
+app.listen(process.env.PORT || 3000)
